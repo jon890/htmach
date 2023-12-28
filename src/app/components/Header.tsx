066c568ec97d81985ae438f7c3db9a1d50045584
bifos <jon89071@gmail.com>
2023-12-28 16:03:53 +0900

    :lipstick: 메인에 영상 변경

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,8 +2,8 @@ const Links = ["기업소개", "사업분야", "기술현황", "실적/사례", 
 
 export default function Header() {
   return (
-    <header className="w-full h-12 py-4 fixed top-0 left-0 z-10">
-      <nav className="py-4 max-w-[1000px] h-full flex items-center mx-auto backdrop-blur-lg saturate-100">
+    <header className="w-full h-12 py-10 sticky top-0 left-0 z-10 backdrop-blur-lg saturate-100 bg-black">
+      <nav className="max-w-[1000px] h-full flex items-center mx-auto">
         <a
           href="/"
           className="bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-pink-500 font-bold text-3xl mr-auto"
@@ -20,7 +20,7 @@ export default function Header() {
 
 function NavLink({ text, href }: { text: string; href: string }) {
   return (
-    <a href={href} className="ml-4">
+    <a href={href} className="ml-4 text-white font-semibold">
       {text}
     </a>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
 import Header from "./components/Header";
-import ScrollSection from "./components/ScrollSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <Header />
 
-      <ScrollSection id={1} />
+      <div className="p-0 w-screen h-screen overflow-hidden absolute">
+        <video
+          id="main-video"
+          src="/videos/bg-main.mp4"
+          preload="none"
+          muted
+          playsInline
+          loop
+          autoPlay
+          className="w-full h-full absolute top-[80px] object-cover"
+        />
+      </div>
+
+      {/* <ScrollSection id={1} /> */}
     </main>
   );
 }
