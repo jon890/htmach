eaa34e730306be849b32240dffdcca06f15a515e
bifos <jon89071@gmail.com>
2023-12-28 16:22:56 +0900

    :lipstick: 영상 위에 글자 추가 및 영상 크기 조정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
-import Header from "./components/Header";
+import Header from "@/app/components/Header";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Header />
 
-      <div className="p-0 w-screen h-screen overflow-hidden absolute">
+      <div className="p-0 w-screen h-[calc(100vh-80px)] overflow-hidden top-[80px] flex justify-center items-center">
+        <div className="absolute z-10 text-white max-w-[1000px] space-y-10">
+          <h1 className="text-5xl font-bold">
+            회전체 기기의 모든 것, HTM이 함께 합니다.
+          </h1>
+          <h2 className="text-lg font-semibold">
+            회전체 기기의 재제조 업무를 기반으로 고객의 요구에 부합할 수 있는
+            최선의 결과를 위해 노력합니다.
+          </h2>
+        </div>
         <video
           id="main-video"
           src="/videos/bg-main.mp4"
@@ -14,7 +23,7 @@ export default function Home() {
           playsInline
           loop
           autoPlay
-          className="w-full h-full absolute top-[80px] object-cover"
+          className="w-full h-full relative object-cover"
         />
       </div>
 
