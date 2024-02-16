2a6e743ac47f15caf30d49193e49734594185505
BiFoS <jon89071@gmail.com>
2024-02-16 12:58:00 +0900

    :art: 상단탭 메뉴 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,19 +4,19 @@ import Link from "next/link";
 import { getMessages } from "@/app/[locale]/get-messages";
 
 const Links = [
-  { langKey: "madyn", link: "/madyn" },
-  { langKey: "company-introduce", link: "#" },
-  { langKey: "business-area", link: "#" },
-  { langKey: "technilogy-status", link: "#" },
-  { langKey: "performance-cases", link: "#" },
-  { langKey: "help", link: "#" },
+  { langKey: "madyn", link: "/madyn", order: 2, visible: true },
+  { langKey: "company-introduce", link: "#", order: 1, visible: true },
+  { langKey: "business-area", link: "#", order: 3, visible: false },
+  { langKey: "technilogy-status", link: "#", order: 4, visible: false },
+  { langKey: "performance-cases", link: "#", order: 5, visible: true },
+  { langKey: "help", link: "#", order: 6, visible: true },
 ] as const;
 
 export default async function Header({ locale }: { locale: "en" | "ko" }) {
   const t = await getMessages(locale);
 
   return (
-    <header className="sticky left-0 top-0 z-20 h-12 w-full bg-navColor py-10">
+    <header className="bg-navColor sticky left-0 top-0 z-20 h-12 w-full py-10">
       <nav className="mx-auto flex h-full max-w-[1000px] items-center">
         <Link
           href="/"
@@ -24,14 +24,16 @@ export default async function Header({ locale }: { locale: "en" | "ko" }) {
         >
           HTM
         </Link>
-        {Links.map(({ langKey, link }) => (
-          <NavLink
-            text={t.Menu[langKey]}
-            href={link}
-            key={langKey}
-            preparingMessage={t.Common.preparing}
-          />
-        ))}
+        {Links.filter((link) => link.visible)
+          .sort((a, b) => a.order - b.order)
+          .map(({ langKey, link }) => (
+            <NavLink
+              text={t.Menu[langKey]}
+              href={link}
+              key={langKey}
+              preparingMessage={t.Common.preparing}
+            />
+          ))}
 
         <LanguageSelect />
       </nav>
