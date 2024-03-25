9ac52de83512b41b989533a28e1944153561d736
BiFoS <jon89071@gmail.com>
2024-03-25 17:54:35 +0900

    :lipstick: 헤더 디자인 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,10 +17,10 @@ export default async function Header({ locale }: { locale: "en" | "ko" }) {
 
   return (
     <header className="fixed left-0 top-0 z-20 h-20 w-full bg-white py-10">
-      <nav className="mx-auto flex h-full max-w-[1000px] items-center">
+      <nav className="mx-auto flex h-full max-w-[1000px] items-center px-4">
         <LinkWithLocale
           href="/"
-          className="mr-auto bg-gradient-to-r from-indigo-500 to-pink-500 bg-clip-text text-3xl font-bold text-transparent"
+          className="mr-auto bg-gradient-to-r from-blue-500 via-gray-500 to-black bg-clip-text text-3xl font-bold text-transparent"
         >
           HTM
         </LinkWithLocale>
