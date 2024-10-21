0b0810fbb2a0cc603007854e99bfad29143b9571
BiFoS <jon89071@gmail.com>
2024-10-21 10:29:39 +0900

    :fire: 사용하지 않는 파일 제거

--- src/components/in-view-helper.tsx
+++ src/components/in-view-helper.tsx
@@ -4,6 +4,12 @@ import classnames from "@/lib/classnames";
 import { ReactNode } from "react";
 import { InView } from "react-intersection-observer";
 
+/**
+ * 해당 엘리먼트가 클라이언트의 뷰(화면)에 나타났을 때
+ * 사용할 클래스를 지정할 수 있는 컴포넌트
+ *
+ * 나타났을 때 애니메이션을 지정하는데 사용한다
+ */
 export default function InViewHelper({
   children,
   anyClass = "",
